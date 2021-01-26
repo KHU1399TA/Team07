acad35c3a65b6be83b7304cf58f01a14dcc36c06
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2021-01-26 16:45:10 +0330

    added main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,32 @@
 package main;
 
-import java.util.Date;
+import java.util.*;
 
 public class Main {
 
-    Date today = new Date();
+    public static void main(String[] args) {
     
-    Manager manager = new Manager("Mohammadmahdi", "Shokraneh", "09302377032", "OmohammadO", "m0012000", MANAGER, today, today);
-    
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("           Hello \n Wellcome to the restaurant");
+        
+        System.out.println("-".repeat(28));
+        
+        System.out.println(" 1)manager 2)cashier 3)chef 4)deliveryman 5)client");
+        
+        System.out.print(" Select your access level's number:");
+
+        byte accesslevelnumber = input.nextByte();
+        
+        switch(accesslevelnumber){
+            
+            case 1 -> manager();
+            case 2 -> System.out.println("-".repeat(28) + " \nplease login");
+            case 3 -> System.out.println("-".repeat(28) + " \nplease login");
+            case 4 -> System.out.println("-".repeat(28) + " \nplease login");
+            default -> System.out.println("");
+        }
+        
+        input.close();
+    }
 }
