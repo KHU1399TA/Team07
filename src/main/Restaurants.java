4df9610e7fb5d9d2b88a74973a3c7073426e1a6c
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2021-01-28 01:18:02 +0330

    add Restaurant class

---
+++ src/main/Restaurants.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.*;
+//import utils.FileManager;
+
+public class Restaurants {
+    
+    static ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+    
+    /*FileManager fileManager = new FileManager("src/resources/users.txt");
+    
+    public void writedata() {
+
+        fileManager.writeLine(users.get(0).show(), false);
+        
+        for (int i = 1; i < users.size(); i++) {
+         
+            fileManager.writeLine(users.get(i).show(), true);
+        }
+    }
+    
+    public void readdata() {
+        
+        for (int i = 1; i > 0; i++) {
+            
+            users.add(fileManager.readLine(i));
+        }
+    }*/
+}
