4c27966a62d10410d677caf075bf4dc755941be2
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2021-01-29 11:17:42 +0330

    added Cashier class

---
+++ src/main/Cashier.java
@@ -0,0 +1,75 @@
+
+package main;
+
+import java.util.Date;
+
+public class Cashier extends User {
+
+    public Cashier(String firstName, String lastName, String phoneNumber,
+            String username, String password, AccessLevel accessLevel,
+            Date registrationDate, Date lastLoginDate) {
+
+        super(firstName, lastName, phoneNumber, username, password, accessLevel,
+                registrationDate, lastLoginDate);
+    }
+    
+    static ActionResult showorders(String restaurantname, String restaurantpass) {
+        
+        for (int i = 0; i < Restaurants.restaurants.size(); i++) {
+
+            if (Restaurants.restaurants.get(i).restaurantname.equals(restaurantname)
+                    && Restaurants.restaurants.get(i).restaurantpass.equals(restaurantpass)) {
+
+                for (int j = 0; j < Restaurants.restaurants.get(i).ordes.size(); j++) {
+
+                    if(Restaurants.restaurants.get(i).ordes.get(j).state.equals(OrderState.MADE)){
+                        
+                        Restaurants.restaurants.get(i).ordes.get(j).toString();
+                    }
+                }
+                        
+                return ActionResult.SUCCESSSHOW;
+            }
+        }
+
+        return ActionResult.RESTAURANT_NOT_FOUND;
+    } 
+    
+    static ActionResult confirmOrder (String restaurantname, String restaurantpass, int orderid) {
+
+        for (int i = 0; i < Restaurants.restaurants.size(); i++) {
+
+            if (Restaurants.restaurants.get(i).restaurantname.equals(restaurantname)
+                    && Restaurants.restaurants.get(i).restaurantpass.equals(restaurantpass)) {
+
+                for (int j = 0; j < Restaurants.restaurants.get(i).ordes.size(); j++) {
+
+                    if (Restaurants.restaurants.get(i).ordes.get(j).id == orderid) {
+
+                        for (int k = 0; k < Restaurants.restaurants.get(i).menu.size(); k++) {
+
+                            if (Restaurants.restaurants.get(i).ordes.get(j).foodId
+                                    == Restaurants.restaurants.get(i).menu.get(k).id) {
+
+                                if (Restaurants.restaurants.get(i).menu.get(k).isAvailable) {
+
+                                    Restaurants.restaurants.get(i).ordes.get(j).state = OrderState.CONFIRMED;
+                                    return ActionResult.SUCCESSCONFIRMED;
+                                }
+
+                                return ActionResult.FOOD_NOT_AVAILABLE;
+                            }
+                        }
+
+                        return ActionResult.FOOD_NOT_FOUND;
+                    }
+
+                }
+
+                return ActionResult.ORDER_NOT_FOUND;
+            }
+        }
+
+        return ActionResult.RESTAURANT_NOT_FOUND;
+    }
+}
